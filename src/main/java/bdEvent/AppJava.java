870da14f2c00187515688d4d7111331ca1172f6b
Alex <sashaalohin@gmail.com>
2019-02-02 12:47:53 +0200

    added initial logic to select input data into dataset.

--- src/main/java/bdEvent/AppJava.java
+++ src/main/java/bdEvent/AppJava.java
@@ -1,15 +1,21 @@
 package bdEvent;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.SparkConf;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 
 public class AppJava {
 
     public static void main(String[] args) {
+    	SparkSession spark = SparkSession.builder().master("local[*]").appName("event").getOrCreate();
+        JavaSparkContext sc = new JavaSparkContext(spark.sparkContext());
 
-        SparkConf conf = new SparkConf().setAppName("event");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-
-       // JavaRDD<String> data = sc.textFile(<<path>>);
-
+		JavaRDD<String> inputFile = sc.textFile("src/main/resources/meteo_data.json.gz");
+		 Dataset<Row> ds = spark.read().json(inputFile);
+		 Dataset<Input> dsData = ds.select("data").as(Encoders.bean(Input.class));
+		 dsData.printSchema();
+		 
     }
 }
---
+++ src/main/java/bdEvent/Input.java
@@ -0,0 +1,17 @@
+package bdEvent;
+
+import java.util.List;
+
+public class Input {
+	private List<Event> data;
+
+	public List<Event> getData() {
+		return data;
+	}
+
+	public void setData(List<Event> data) {
+		this.data = data;
+	}
+
+	
+}
